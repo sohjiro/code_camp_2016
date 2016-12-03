0f646fc23f351805383687cb7fb8ec6b1fbda77d
misaelpc <misaelpcgm@gmail.com>
2016-12-03 13:23:37 -0600

    merge

--- lib/code_camp_2016/twitter/CLI.ex
+++ lib/code_camp_2016/twitter/CLI.ex
@@ -5,15 +5,14 @@ defmodule CodeCamp2016.Twitter.CLI do
     |> ExTwitter.follower_ids
   end
 
-<<<<<<< HEAD
   def screen_name(user_id) do
     user_id
     |> ExTwitter.user
-=======
+  end
+
   def friends(username) do
     username
     |> ExTwitter.friend_ids
->>>>>>> 1b8e8f8d2ffa7241853d652b1b957cf84d763580
   end
 
 end
