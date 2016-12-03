b6a10748647f9d03b92471c0e7c696e71e4bdb71
misaelpc <misaelpcgm@gmail.com>
2016-12-03 13:37:52 -0600

    Prod Config

--- config/prod.exs
+++ config/prod.exs
@@ -28,6 +28,13 @@ config :hello_phoenix, HelloPhoenix.Repo,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :extwitter, :oauth, [
+  consumer_key: "HAZYB48Y7amAK2XHiFeRa3Urd",
+  consumer_secret: "MZcmSvbP1iJ4lGR8oyNMc5sanNdy7WQ36AEgwUtQwHvCxFBuBe",
+  access_token: "29611045-VkarDR2Rv0pkn15S5xYVvJkVdIO16Zczzqz5H10r6",
+  access_token_secret: "m6iG0cqOn3PbCe9o5IpTmaINooiOAUNNnL1F6Ecdoo3mP"
+]
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
