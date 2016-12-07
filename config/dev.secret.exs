cca93ef4019cae81d69998ce929489a5685ee7c2
Felipe Juárez <felipe@makingdevs.com>
2016-12-07 15:09:09 -0600

    Delete dev.secret.exs

--- config/dev.secret.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-#Twiiter config
-config :extwitter, :oauth, [
-  consumer_key: "BG0apPvJdtXh5goOywEoLg",
-  consumer_secret: "wRVxNmYpW63fDoXawkCva9RcrIf5R3riYBBDNOUkCc",
-  access_token: "63298457-rlgLzbO5nE3hcZOP4urEmMpqUjY2y3acfpbbmYnn3",
-  access_token_secret: "1oNq6SXTcxctf4KOAMUTJHEbwYEQng8OhxA1ylDc9s"
-]
